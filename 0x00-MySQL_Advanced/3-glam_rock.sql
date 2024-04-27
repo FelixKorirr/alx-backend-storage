beff7db33fc16a10886832c0ccce39b64220f461
Felix Kipkorir <kipronofelix63@gmail.com>
2024-04-27 08:51:25 +0300

    MySQL_Advanced

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Lists all bands with glam rock as their main style
 SELECT band_name, (COALESCE(split, '2020') - formed) AS lifespan
 FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', style)
+WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 ORDER BY lifespan DESC
