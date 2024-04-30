ec5ac8dbb8e0d2c5babd0a865cad327612590bd6
Felix Kipkorir <kipronofelix63@gmail.com>
2024-04-30 22:25:07 +0300

    NoSQL

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,13 +4,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """Inserts a new document in a collection"""
-    my_dict = {}
-    for key, value in kwargs.items():
-        my_dict[key] = value
-
-    mongo_collection.insert_one(my_dict)
-
-    new_document = mongo_collection.find_one({})
-    id_of_document = new_document['_id']
-
-    return id_of_document
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
