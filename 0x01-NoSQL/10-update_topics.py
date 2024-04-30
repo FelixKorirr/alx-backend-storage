50086a54ce68f0f388515c0c525b8ff54158feaf
Felix Kipkorir <kipronofelix63@gmail.com>
2024-04-30 22:37:48 +0300

    NoSQL

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''module 10'''
+
+
+def update_topics(mongo_collection, name, topics):
+    '''Changes all topics of a school document based on name'''
+    mongo_collection.update_one(
+        { 'name' : name }, { '$set': { 'topics' : topics } }
+    )
