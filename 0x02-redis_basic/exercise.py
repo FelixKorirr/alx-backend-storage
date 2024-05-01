f214a5d2db5e75038ed2224fd35de827efda51fd
Felix Kipkorir <kipronofelix63@gmail.com>
2024-05-01 17:17:39 +0300

    Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
-"""Redis basics"""
+'''Redis basics'''
 import uuid
 import redis
 
 
 class Cache:
-    """Represents Cache class"""
+    '''Represents Cache class'''
 
     def __init__(self):
-        """Initialize Cache class"""
+        '''Initialize Cache class'''
         self._redis = redis.Redis()
 
     def store(self, data):
-        """Generates random key"""
+        '''Generates random key'''
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
 
     def get(key):
-        """Decodes redis data return type
-            to desired format"""
+        '''Decodes redis data return type
+        to desired format'''
         return key.decode('utf-8')
